a8f58e2b6a10ac209ce11139f9a238a37eff26e0
sagar s <41886209+Sagar0802@users.noreply.github.com>
2020-08-17 12:35:33 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ public class helloworld
 {
   public static void main(String[] args) {
         System.out.println("Hello folks Welcome to new World..\n Happy Coding ");
+        System.out.println("Hello Nikitha Welcome to new World..\n Happy Coding ");
   }
 }
